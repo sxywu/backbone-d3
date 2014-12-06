3888d9fdeaa14f4b7aef4f3f1a318f9c203ce8fb
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-12-06 14:12:49 -0800

    convert tab to spaces

--- scripts/backbone01.js
+++ scripts/backbone01.js
@@ -7,87 +7,87 @@ written only with Backbone
 
 // first, "fetch" the data into a collection
 var PartyCollection = Backbone.Collection.extend({
-	url: "data/party.json",
-	getAllTimes: function() {
-		return this.chain().pluck('attributes')
-			.pluck('time').uniq().value();
-	},
-	getPartiersAtTime: function(time) {
-		return this.chain().groupBy(function(model) {
-			return model.get('partier')
-		}).filter(function(actions) {
-			return actions[0].get('time') <= time
-				&& (actions[1] ? time < actions[1].get('time') : true);
-		}).map(function(actions) {
-			return {
-				name: actions[0].get('partier'),
-				entered: actions[0].get('time') === time,
-				exit: actions[1] && (actions[1].get('time') - 100 === time)
-			};
-		}).value();
-	}
+  url: "data/party.json",
+  getAllTimes: function() {
+    return this.chain().pluck('attributes')
+      .pluck('time').uniq().value();
+  },
+  getPartiersAtTime: function(time) {
+    return this.chain().groupBy(function(model) {
+      return model.get('partier')
+    }).filter(function(actions) {
+      return actions[0].get('time') <= time
+        && (actions[1] ? time < actions[1].get('time') : true);
+    }).map(function(actions) {
+      return {
+        name: actions[0].get('partier'),
+        entered: actions[0].get('time') === time,
+        exit: actions[1] && (actions[1].get('time') - 100 === time)
+      };
+    }).value();
+  }
 });
 
 var PartyView = Backbone.View.extend({
-	initialize: function() {
-		this.collection = this.options.collection;
-		this.selectedTime;
+  initialize: function() {
+    this.collection = this.options.collection;
+    this.selectedTime;
 
-		this.collection.on('reset', _.bind(this.render, this));
-	},
-	// then render two parts:
-	// 1. the time
-	// 2. the partiers at that time
-	render: function() {
-		var times = this.collection.getAllTimes();
-		this.selectedTime = this.selectedTime || times[0];
-		var partiers = this.collection.getPartiersAtTime(this.selectedTime);
+    this.collection.on('reset', _.bind(this.render, this));
+  },
+  // then render two parts:
+  // 1. the time
+  // 2. the partiers at that time
+  render: function() {
+    var times = this.collection.getAllTimes();
+    this.selectedTime = this.selectedTime || times[0];
+    var partiers = this.collection.getPartiersAtTime(this.selectedTime);
 
-		this.$el.html(this.renderTime(times));
-		this.$el.append(this.renderPartiers(partiers));
+    this.$el.html(this.renderTime(times));
+    this.$el.append(this.renderPartiers(partiers));
 
-		return this;
-	},
-	renderTime: function(times) {
-		var $times = $('<div class="times"></div>'),
-			that = this;
-		_.each(times, function(time) {
-			$times.append('<span class="btn '
-				+ (that.selectedTime === time ? 'btn-primary' : 'btn-default')
-				+ ' btn-xs partyTime">' 
-				+ time
-			+ '</span>');
-		});
-		return $times;
-	},
-	renderPartiers: function(partiers) {
-		var $partiers = $('<span class="partiers"></span>');
-		_.each(partiers, function(partier) {
-			$partiers.append('<span class="label ' 
-				+ (partier.entered ? 'label-primary ' : '')
-				+ (partier.exit ? 'label-warning ': '')
-				+ (!partier.entered && !partier.exit ? 'label-default ': '')
-				+ 'partier">'
-				+ partier.name + '</span>');
-		});
-		return $partiers;
-	},
-	events: {
-		'click .partyTime': 'update'
-	},
-	// updating is just rerendering 
-	update: function(e) {
-		this.selectedTime = parseInt($(e.target).text());
+    return this;
+  },
+  renderTime: function(times) {
+    var $times = $('<div class="times"></div>'),
+      that = this;
+    _.each(times, function(time) {
+      $times.append('<span class="btn '
+        + (that.selectedTime === time ? 'btn-primary' : 'btn-default')
+        + ' btn-xs partyTime">' 
+        + time
+      + '</span>');
+    });
+    return $times;
+  },
+  renderPartiers: function(partiers) {
+    var $partiers = $('<span class="partiers"></span>');
+    _.each(partiers, function(partier) {
+      $partiers.append('<span class="label ' 
+        + (partier.entered ? 'label-primary ' : '')
+        + (partier.exit ? 'label-warning ': '')
+        + (!partier.entered && !partier.exit ? 'label-default ': '')
+        + 'partier">'
+        + partier.name + '</span>');
+    });
+    return $partiers;
+  },
+  events: {
+    'click .partyTime': 'update'
+  },
+  // updating is just rerendering 
+  update: function(e) {
+    this.selectedTime = parseInt($(e.target).text());
 
-		this.render();
-	}
+    this.render();
+  }
 });
 
 
 var partyCollection = new PartyCollection();
 var partyView = new PartyView({
-	el: $('.party'),
-	collection: partyCollection
+  el: $('.party'),
+  collection: partyCollection
 });
 partyCollection.fetch({reset: true});
 
--- scripts/d301.js
+++ scripts/d301.js
@@ -1,112 +1,112 @@
 // fetch data
 d3.json('data/party.json', function(response) {
-	var getAllTimes = function(actions) {
-			return _.chain(actions).pluck('time').uniq().value();
-		},
-		getPartiersAtTime = function(actions, time) {
-			return _.chain(actions).groupBy(function(action) {
-				return action.partier;
-			}).filter(function(actions) {
-				return actions[0].time <= time
-					&& (actions[1] ? time < actions[1].time : true);
-			}).map(function(actions) {
-				return {
-					name: actions[0].partier,
-					entered: actions[0].time === time,
-					exit: actions[1] && (actions[1].time - 100 === time)
-				};
-			}).value();
-		}
+  var getAllTimes = function(actions) {
+      return _.chain(actions).pluck('time').uniq().value();
+    },
+    getPartiersAtTime = function(actions, time) {
+      return _.chain(actions).groupBy(function(action) {
+        return action.partier;
+      }).filter(function(actions) {
+        return actions[0].time <= time
+          && (actions[1] ? time < actions[1].time : true);
+      }).map(function(actions) {
+        return {
+          name: actions[0].partier,
+          entered: actions[0].time === time,
+          exit: actions[1] && (actions[1].time - 100 === time)
+        };
+      }).value();
+    }
 
-	var allTimes = getAllTimes(response),
-		selectedTime = allTimes[0],
-		partiersAtTime = getPartiersAtTime(response, selectedTime);
+  var allTimes = getAllTimes(response),
+    selectedTime = allTimes[0],
+    partiersAtTime = getPartiersAtTime(response, selectedTime);
 
-	var container = d3.select('.party');
+  var container = d3.select('.party');
 
-	// first render the times
-	var times = container.append('div')
-		.classed('times', true)
-		.selectAll('.partyTime')
-		.data(getAllTimes(response));
-		
-	times.enter().append('span')
-			.classed({
-				'partyTime': true,
-				'btn': true,
-				'btn-xs': true,
-				'btn-default': function(time) {
-					return time !== selectedTime;
-				},
-				'btn-primary': function(time) {
-					return time === selectedTime;
-				}
-			}).text(function(time) {
-				return time;
-			});
+  // first render the times
+  var times = container.append('div')
+    .classed('times', true)
+    .selectAll('.partyTime')
+    .data(getAllTimes(response));
+    
+  times.enter().append('span')
+      .classed({
+        'partyTime': true,
+        'btn': true,
+        'btn-xs': true,
+        'btn-default': function(time) {
+          return time !== selectedTime;
+        },
+        'btn-primary': function(time) {
+          return time === selectedTime;
+        }
+      }).text(function(time) {
+        return time;
+      });
 
-	// then render the partiers
-	var partiers = container.append('div')
-		.classed('partiers', true)
-		.selectAll('.partier')
-		.data(partiersAtTime, function(partier) {
-			return partier.name;
-		});
+  // then render the partiers
+  var partiers = container.append('div')
+    .classed('partiers', true)
+    .selectAll('.partier')
+    .data(partiersAtTime, function(partier) {
+      return partier.name;
+    });
 
-	partiers.enter().append('span')
-			.classed({
-				'partier': true,
-				'label': true,
-				'label-default': function(partier) {
-					return !partier.entered && !partier.exit;
-				},
-				'label-primary': function(partier) {
-					return partier.entered;
-				},
-				'label-warning': function(partier) {
-					return partier.exit;
-				}
-			}).text(function(partier) {
-				return partier.name;
-			});
+  partiers.enter().append('span')
+      .classed({
+        'partier': true,
+        'label': true,
+        'label-default': function(partier) {
+          return !partier.entered && !partier.exit;
+        },
+        'label-primary': function(partier) {
+          return partier.entered;
+        },
+        'label-warning': function(partier) {
+          return partier.exit;
+        }
+      }).text(function(partier) {
+        return partier.name;
+      });
 
-	times.on('click', function() {
-		selectedTime = d3.select(d3.event.target).datum();
-		partiersAtTime = getPartiersAtTime(response, selectedTime);
+  times.on('click', function() {
+    selectedTime = d3.select(d3.event.target).datum();
+    partiersAtTime = getPartiersAtTime(response, selectedTime);
 
-		times.classed({
-			'btn-default': function(time) {
-					return time !== selectedTime;
-			},
-			'btn-primary': function(time) {
-				return time === selectedTime;
-			}
-		})
+    times.classed({
+      'btn-default': function(time) {
+          return time !== selectedTime;
+      },
+      'btn-primary': function(time) {
+        return time === selectedTime;
+      }
+    })
 
-		// rebind the updated partier data
-		partiers = partiers.data(partiersAtTime, function(partier) {
-				return partier.name;
-			})
+    // rebind the updated partier data
+    partiers = partiers.data(partiersAtTime, function(partier) {
+        return partier.name;
+      })
 
-		partiers.enter().append('span');
+    partiers.enter().append('span');
 
-		partiers.exit().remove();
+    partiers.exit().remove();
 
-		// update
-		partiers.classed({
-				'partier': true,
-				'label': true,
-				'label-default': function(partier) {
-					return !partier.entered && !partier.exit;
-				},
-				'label-primary': function(partier) {
-					return partier.entered;
-				},
-				'label-warning': function(partier) {
-					return partier.exit;
-				}
-			}).text(function(partier) {
-				return partier.name;
-			});
-	})
+    // update
+    partiers.classed({
+        'partier': true,
+        'label': true,
+        'label-default': function(partier) {
+          return !partier.entered && !partier.exit;
+        },
+        'label-primary': function(partier) {
+          return partier.entered;
+        },
+        'label-warning': function(partier) {
+          return partier.exit;
+        }
+      }).text(function(partier) {
+        return partier.name;
+      });
+  })
 });
