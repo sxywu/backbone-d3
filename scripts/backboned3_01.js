1bb2d731dccc50046b418e0f2d3db2bf731e39f7
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-12-06 15:58:08 -0800

    TimeView and GraphView

--- scripts/backboned3_01.js
+++ scripts/backboned3_01.js
@@ -7,50 +7,70 @@ var PartyCollection = Backbone.Collection.extend({
   save: function() {
     localStorage['party'] = JSON.stringify(this.toJSON());
   },
+  getSelectedTime: function() {
+    return this.selectedTime || this.getAllTimes()[0] || 1800;
+  },
+  setSelectedTime: function(time) {
+    this.selectedTime = time;
+  },
   getAllTimes: function() {
     return this.chain().pluck('attributes')
       .pluck('time').uniq().value();
   },
-  getPartiersAtTime: function(time) {
-    return this.chain().groupBy(function(model) {
-      return model.get('partier')
-    }).filter(function(actions) {
-      return actions[0].get('time') <= time
-        && (actions[1] ? time < actions[1].get('time') : true);
-    }).map(function(actions) {
-      return {
-        name: actions[0].get('partier'),
-        entered: actions[0].get('time') === time,
-        exit: actions[1] && (actions[1].get('time') - 100 === time)
-      };
-    }).value();
+  getGraphAtTime: function(time) {
+    var nodes = this.chain().groupBy(function(model) {
+        return model.get('partier')
+      }).filter(function(actions) {
+        var enter = _.find(actions, function(action) {
+            return action.get('action') === 'enter';
+          }),
+          exit = _.find(actions, function(action) {
+            return action.get('action') === 'exit';
+          });
+        return enter.get('time') <= time
+          && (exit ? time < exit.get('time') : true);
+      }).map(function(actions) {
+        var enter = _.find(actions, function(action) {
+            return action.get('action') === 'enter';
+          }),
+          exit = _.find(actions, function(action) {
+            return action.get('action') === 'exit';
+          });
+        return {
+          name: enter.get('partier'),
+          entered: enter.get('time') === time,
+          exit: exit && (exit.get('time') - 100 === time)
+        };
+      }).value();
+
+    var links = this.chain().filter(function(model) {
+        return model.get('action') === 'talk';
+      }).map(function(talk) {
+        return {
+          source: _.find(nodes, function(node) {
+            return node.name === talk.get('partier');
+          }),
+          target: _.find(nodes, function(node) {
+            return node.name === talk.get('partier2');
+          })
+        }
+      }).value();
+
+      return {nodes: nodes, links: links};
   }
 });
 
 var AppView = Backbone.View.extend({
   initialize: function() {
     this.collection = this.options.collection;
-    this.selectedTime;
-
-    this.collection.on('reset add remove', _.bind(this.render, this));
-  },
-  render: function() {
-    var times = this.collection.getAllTimes();
-    this.selectedTime = this.selectedTime || times[0];
-
-    this.$('.results').html(this.renderTime(times));
-  },
-  renderTime: function(times) {
-    var $times = $('<div class="times"></div>'),
-      that = this;
-    _.each(times, function(time) {
-      $times.append('<span class="btn '
-        + (that.selectedTime === time ? 'btn-primary' : 'btn-default')
-        + ' btn-xs partyTime">' 
-        + time
-      + '</span>');
+    this.graphView = new GraphView({
+      el: this.$('.graph'),
+      collection: this.collection
     });
-    return $times;
+    this.timeView = new TimeView({
+      el: this.$('.times'),
+      collection: this.collection
+    })
   },
   events: {
     "click .submitParty": "submitParty",
@@ -80,13 +100,120 @@ var AppView = Backbone.View.extend({
     } else {
       this.$('.inputPartier2').addClass('hidden');
     }
+  }
+});
+
+var TimeView = Backbone.View.extend({
+  initialize: function() {
+    this.collection = this.options.collection;
+    this.selectedTime;
+
+    this.collection.on('reset add remove', _.bind(this.render, this));
+  },
+  render: function() {
+    var times = this.collection.getAllTimes();
+    this.$el.empty();
+    this.renderTime(times);
+  },
+  renderTime: function(times) {
+    var that = this, 
+      selectedTime = this.collection.getSelectedTime();
+    _.each(times, function(time) {
+      that.$el.append('<span class="btn '
+        + (selectedTime === time ? 'btn-primary' : 'btn-default')
+        + ' btn-xs partyTime">' 
+        + time
+      + '</span>');
+    });
   },
   updateTime: function(e) {
   }
 });
 
 var GraphView = Backbone.View.extend({
-  
+  initialize: function() {
+    this.collection = this.options.collection;
+    this.collection.on('reset add remove', _.bind(this.render, this));
+
+    this.d3 = d3.select(this.el);
+    this.force = d3.layout.force()
+      .size([250, 250])
+      .charge(-300)
+      .linkDistance(100)
+      .on('tick', _.bind(this.onTick, this));
+  },
+  render: function() {
+    var selectedTime = this.collection.getSelectedTime(),
+      graph = this.collection.getGraphAtTime(selectedTime);
+
+    this.renderNodes(graph.nodes);
+    this.renderLinks(graph.links);
+    this.updateForce(graph.nodes, graph.links);
+  },
+  renderNodes: function(nodes) {
+    this.node = this.d3.selectAll('.node')
+      .data(nodes, function(d) {
+        return d.name;
+      });
+
+    this.node.enter().append('g')
+      .classed('node', true);
+    this.node.exit().remove();
+
+    this.node.append('circle')
+        .attr('r', 10)
+        .attr('fill', function(d) {
+          console.log(d)
+          return d.entered ? '#337ab7' :
+            (d.exit ? '#f0ad4e' : '#fff');
+        }).attr('stroke', function(d) {
+          return d.entered ? '#2e6da4' : 
+            (d.exit ? '#eea236' : '#ccc');
+        }).attr('stroke-width', 2);
+
+    this.node.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.35em')
+      .attr('fill', function(d) {
+        return (d.entered || d.exit) ? '#fff' : '#ccc';
+      })
+      .text(function(d) {
+        return d.name[0];
+      });
+  },
+  renderLinks: function(links) {
+    this.link = this.d3.selectAll('.link')
+      .data(links, function(d) {
+        return d.source.name + ',' + d.target.name;
+      });
+
+    this.link.enter().insert('line', '.node')
+      .classed('link', true);
+    this.link.exit().remove();
+
+    this.link.attr('stroke', '#ccc')
+      .attr('stroke-width', 2);
+  },
+  updateForce: function(nodes, links) {
+    this.force.nodes(nodes)
+      .links(links);
+
+    this.force.start();
+  },
+  onTick: function() {
+    this.node.attr('transform', function(d) {
+      return 'translate(' + d.x + ',' + d.y + ')';
+    });
+    this.link.attr('x1', function(d) {
+      return d.source.x;
+    }).attr('y1', function(d) {
+      return d.source.y;
+    }).attr('x2', function(d) {
+      return d.target.x;
+    }).attr('y2', function(d) {
+      return d.target.y;
+    });
+  }
 });
 
 var partyCollection = new PartyCollection();
