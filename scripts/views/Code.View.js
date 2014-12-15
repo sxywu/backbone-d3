3e4c0a4d8d0da4ecedb3b144fbef9a7edadf0190
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-12-15 02:46:53 -0500

    concise mode success!

--- scripts/views/Code.View.js
+++ scripts/views/Code.View.js
@@ -14,20 +14,32 @@ define([
       this.lines = this.options.lines;
       this.sections = this.options.sections;
       this.highlights = this.options.highlights;
+      this.toggleHighlight = false;
     },
     render: function() {
       var template = _.template(CodeTemplate, {
         sections: this.sections, 
         lines: this.lines,
         highlights: this.highlights,
-        toggleHighlight: true
+        toggleHighlight: this.toggleHighlight
       });
 
-      this.$el.html(template);
+      this.$('.codeSample').html(template);
 
       Prism.highlightAll();
 
       return this;
+    },
+    events: {
+      "click .conciseMode": "toggleMode"
+    },
+    toggleMode: function() {
+      this.toggleHighlight = !this.toggleHighlight;
+
+      this.$('.conciseMode').toggleClass('btn-default');
+      this.$('.conciseMode').toggleClass('btn-primary');
+
+      this.render();
     }
   });
 })
